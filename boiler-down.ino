e13713166238ee1ffeca622a10d804a28fc5a7f6
NikS <arduin.inmail@gmail.com>
2019-02-04 18:32:54 +0200

    редактирование 156

--- boiler-down.ino
+++ boiler-down.ino
@@ -153,7 +153,7 @@ void realTimeService() {
 
   reqClient.println(F("HTTP/1.1 200 OK"));
   reqClient.println(F("Content-Type: application/json"));
-  reqClient.println(F("Content-Length: "));
+  reqClient.print(F("Content-Length: "));
   reqClient.println(data.length());
   reqClient.println();
   reqClient.print(data);
